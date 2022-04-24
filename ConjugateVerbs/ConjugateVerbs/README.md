6f6a258d171e364cdc5a68c60d276b4943f0e75f
AntonReus <mhernandezn1602@alumno.ipn.mx>
2022-04-24 08:50:53 -0500

    .

---
+++ ConjugateVerbs/ConjugateVerbs/README.md
@@ -0,0 +1,2 @@
+# ConjugateVerbs
+School project, application for the study of English conjugating verbs in the present simple
---
+++ ConjugateVerbs/ConjugateVerbs/cSubjet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class cSubjet
+    {
+        public string id_subjet { get; set; }
+        public string Subjet { get; set; }
+    }
+}
